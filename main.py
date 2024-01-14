88cc016aaa2bde11876764bf408384cb55046cfd
blueprintparadise <rhiray03@gmail.com>
2024-01-15 00:48:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ def create_app(config=None):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("-p", "--port", action="store", default="8000")
+    parser.add_argument("-p", "--port", action="store", default="3000")
 
     args = parser.parse_args()
     port = int(args.port)
