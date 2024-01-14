36e31ed413be1220c27afc457e219a71a6d17ef4
blueprintparadise <rhiray03@gmail.com>
2024-01-15 00:45:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,38 @@
-from flask import Flask
-helloworld = Flask(__name__)
-@helloworld.route ("/")
-def run ():
-    return "{\"message\":\"Hey there Python\"}"
-    
-@helloworld.route ("/test")
-def test():
-    return "{\"message\":\"Hey there \"}"
-    
+import os
+import argparse
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+
+def create_app(config=None):
+    app = Flask(__name__)
+
+    # See http://flask.pocoo.org/docs/latest/config/
+    app.config.update(dict(DEBUG=True))
+    app.config.update(config or {})
+
+    # Setup cors headers to allow all domains
+    # https://flask-cors.readthedocs.io/en/latest/
+    CORS(app)
+
+    # Definition of the routes. Put them into their own file. See also
+    # Flask Blueprints: http://flask.pocoo.org/docs/latest/blueprints
+    @app.route("/")
+    def hello_world():
+        return "Hello World"
+
+    @app.route("/foo/<someId>")
+    def foo_url_arg(someId):
+        return jsonify({"echo": someId})
+
+    return app
+
+
 if __name__ == "__main__":
-    helloworld.run(host="0.0.0.0", port=int("3000"), debug= True)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-p", "--port", action="store", default="8000")
+
+    args = parser.parse_args()
+    port = int(args.port)
+    app = create_app()
+    app.run(host="0.0.0.0", port=port)
