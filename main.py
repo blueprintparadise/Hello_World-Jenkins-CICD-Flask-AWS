11b1afa6ac5259ccf605f3c773a385e2b318658c
blueprintparadise <rhiray03@gmail.com>
2024-01-14 20:19:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,1 @@
-from flask import Flask
-helloworld = Flask(__name__)
-print("INSIDE THE MAIN _______________________________________")
-@helloworld.route ("/")
-def run ():
-    return "{\"message\":\"Hey there Python\"}"
 
-if __name__ == "__main__":
-    helloworld.run(host="0.0.0.0", port=int("3000"), debug= True)
