e308a1a69ede691e75fa62dcac8865e27fda310e
blueprintparadise <rhiray03@gmail.com>
2024-01-14 20:27:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ helloworld = Flask(__name__)
 @helloworld.route ("/")
 def run ():
     return "{\"message\":\"Hey there Python\"}"
-
+    
+@helloworld.route ("/test")
+def test():
+    return "{\"message\":\"Hey there \"}"
+    
 if __name__ == "__main__":
     helloworld.run(host="0.0.0.0", port=int("3000"), debug= True)
